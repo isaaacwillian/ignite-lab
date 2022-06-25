ae9691933b26a7a212e620c3da6bf58d0c8df9c2
Isaac Willian <isaaacwillian@gmail.com>
2022-06-24 23:17:11 -0300

    First class done and some of second

---
+++ src/components/Video.tsx
@@ -0,0 +1,23 @@
+export function Video() {
+  return (
+    <div className="flex-1">
+      <div className="bg-black flex justify-center">
+        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
+      </div>
+
+      <div className="p-8 max-w-[1100px] mx-auto">
+        <div className="flex items-start gap-16">
+          <div className="flex-1">
+            <h1>Aula 01 - Abertura do Ignite Lab</h1>
+            <p>
+              WWFIWEJIO EJOFIREHGIRHGIUUGIHREIGHIEOROJGOI REJGIE
+              RGISENRIGNIERGHIUEHN DIURHG IUSEHN UIGERGI UAHNIRGUNER
+            </p>
+          </div>
+
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+}
